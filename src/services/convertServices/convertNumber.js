a062e18a8de92a781b450f3fd4571f1dac8406a2
Jean <jeanduarte322@gmail.com>
2022-04-07 16:09:14 -0300

    Refazendo a lógica da conversão

--- src/services/convertServices/convertNumber.js
+++ src/services/convertServices/convertNumber.js
@@ -4,13 +4,16 @@ export const ConveterDecimalForBinary = ({ decimal }) => {
     let quociente = validationDecimal
     const division = []
 
-    while (quociente > 2) {
+    while (quociente >= 2) {
         const resto = quociente % 2
         division.push(resto)
+        console.log("quociente", quociente)
+        console.log("resto = ", resto)
         quociente = Math.floor(quociente / 2)
     }
     if (quociente == 1 || quociente == 0){
         division.push(quociente)
+        console.log("ultimo quociente = ", quociente)
     }
 
     const binary = division.slice(0).reverse();
