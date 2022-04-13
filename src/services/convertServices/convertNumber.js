3b9d0bdda3eb59ee857b5a37695f128d17014acd
Jean <jeanduarte322@outlook.com>
2022-04-13 11:45:13 -0300

    Otimizando o service

--- src/services/convertServices/convertNumber.js
+++ src/services/convertServices/convertNumber.js
@@ -1,22 +1,17 @@
 export const ConveterDecimalForBinary = ({ decimal }) => {
   const validationDecimal = isNaN(decimal) ? 0 : decimal;
-
   let quociente = validationDecimal;
+
   const division = [];
 
   while (quociente >= 2) {
     const resto = quociente % 2;
     division.push(resto);
-    console.log("quociente", quociente);
-    console.log("resto = ", resto);
     quociente = Math.floor(quociente / 2);
   }
   division.push(quociente);
-  console.log("ultimo quociente = ", quociente);
 
-  console.log("Sequencia do array ficou = ",division)
   const binary = division.slice(0).reverse();
-  console.log("Invertendo a divisão fica = ", binary);
 
   return binary;
 };
