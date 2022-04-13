3d310f2c1684bb7dc53e70a8545a89f8165745f9
Jean <jeanduarte322@outlook.com>
2022-04-13 11:43:00 -0300

    Limitando input apenas para numeros

--- src/components/inputNumber/index.jsx
+++ src/components/inputNumber/index.jsx
@@ -1,12 +1,23 @@
 import "./styles.css";
 
+window.addEventListener("keypress", (e) => {
+  if (!checkChar(e)) e.preventDefault();
+});
+
+function checkChar(e) {
+  const char = String.fromCharCode(e.keyCode);
+  const pattern = "[0-9]";
+  if (char.match(pattern)) return true;
+}
+
 export const InputNumber = ({ onChange, size, minHeight }) => (
   <div className="inputBorder">
     <div className="inputDivNumber">
       <input
         type="search"
+        maxLength={15}
         autoFocus
-        className="inputNumber1"
+        className="inputNumber"
         onChange={onChange}
         placeholder="Digite um número decimal"
         style={{ fontSize: size, minHeight: minHeight }}
