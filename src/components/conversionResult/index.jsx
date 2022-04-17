7cac67c27a3b7acfb09fd21b63f16168e3fa09c3
Jean <jeanduarte322@outlook.com>
2022-04-17 10:20:23 -0300

    Criei uma Operação ternária

--- src/components/conversionResult/index.jsx
+++ src/components/conversionResult/index.jsx
@@ -1,11 +1,14 @@
 import "./styles.css";
 
-export const ConversionResult = ({ result, size, minHeight }) => (
-  <>
+export const ConversionResult = ({ result, size, minHeight, flag }) => {
+  const state = flag ? "visible" : "hidden";
+
+  return (
     <div className="inputBorder">
+      <div className="animation" style={{ visibility: state }}></div>
       <div className="result" style={{ fontSize: size, minHeight: minHeight }}>
         {result}
       </div>
     </div>
-  </>
-);
+  );
+};
